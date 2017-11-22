ee86cd9e28fc3c370bba18705f5aa304b7cd2a46
贾博瑄 <jiaboxuan@satcatche.com>
2017-11-22 13:51:26 +0800

    feat:更新布局

--- app/src/androidTest/java/com/jiaye/loan/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/jiaye/loan/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.jiaye.loan", appContext.getPackageName());
+        assertEquals("com.jiaye.cashloan", appContext.getPackageName());
     }
 }
