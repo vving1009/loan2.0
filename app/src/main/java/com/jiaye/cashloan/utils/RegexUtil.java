6f9d760d73f971262f3690ffb35e9e4e1a6331c8
贾博瑄 <jiaboxuan@satcatche.com>
2017-12-12 10:46:55 +0800

    fix:修改手机号正则规则

--- app/src/main/java/com/jiaye/cashloan/utils/RegexUtil.java
+++ app/src/main/java/com/jiaye/cashloan/utils/RegexUtil.java
@@ -9,7 +9,7 @@ package com.jiaye.cashloan.utils;
 public class RegexUtil {
 
     public static String phone() {
-        return "^((13[0-9])|(14[0,9])|(15[0,9])|(17[0,9])|(18[0,9]))\\d{8}$";
+        return "^((13[0-9])|(14[0-9])|(15[0-9])|(17[0-9])|(18[0-9]))\\d{8}$";
     }
 
     public static String email() {
