d00a5ea3e25604ee7ab700c6b608885b28ac317f
贾博瑄 <jiaboxuan@satcatche.com>
2018-01-18 10:34:05 +0800

    fix:#102解决用户在其他机器登录后,当前机器被迫下线后的崩溃问题

--- app/src/main/java/com/jiaye/cashloan/view/view/main/MainActivity.java
+++ app/src/main/java/com/jiaye/cashloan/view/view/main/MainActivity.java
@@ -1,10 +1,12 @@
 package com.jiaye.cashloan.view.view.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 
 import com.jiaye.cashloan.R;
 import com.jiaye.cashloan.view.BaseActivity;
+import com.jiaye.cashloan.view.view.auth.AuthActivity;
 
 public class MainActivity extends BaseActivity {
 
@@ -15,5 +17,13 @@ public class MainActivity extends BaseActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
         MainFragment fragment = MainFragment.newInstance();
         fragmentManager.beginTransaction().replace(R.id.layout_content, fragment).commit();
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String type = bundle.getString("type","default");
+            if (type.equals("reLogin")) {
+                Intent intent = new Intent(this, AuthActivity.class);
+                startActivity(intent);
+            }
+        }
     }
 }
