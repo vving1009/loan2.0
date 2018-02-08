b7086a539e418bbdc7c8330707781c36d94a51d7
贾博瑄 <jiaboxuan@satcatche.com>
2018-02-08 17:34:58 +0800

    fix:补写忘记添加的错误处理

--- app/src/main/java/com/jiaye/cashloan/view/view/my/certificate/bank/BankPresenter.java
+++ app/src/main/java/com/jiaye/cashloan/view/view/my/certificate/bank/BankPresenter.java
@@ -2,6 +2,7 @@ package com.jiaye.cashloan.view.view.my.certificate.bank;
 
 import com.jiaye.cashloan.http.data.my.Bank;
 import com.jiaye.cashloan.view.BasePresenterImpl;
+import com.jiaye.cashloan.view.ThrowableConsumer;
 import com.jiaye.cashloan.view.ViewTransformer;
 import com.jiaye.cashloan.view.data.my.certificate.bank.source.BankDataSource;
 
@@ -29,7 +30,7 @@ public class BankPresenter extends BasePresenterImpl implements BankContract.Pre
     public void subscribe() {
         super.subscribe();
         Disposable disposable = mDataSource.bank()
-                .compose(new ViewTransformer<Bank>(){
+                .compose(new ViewTransformer<Bank>() {
                     @Override
                     public void accept() {
                         super.accept();
@@ -63,7 +64,7 @@ public class BankPresenter extends BasePresenterImpl implements BankContract.Pre
                         String endNumber = number.substring(12, 16);
                         mView.setNumber(startNumber + "*********" + endNumber);
                     }
-                });
+                }, new ThrowableConsumer(mView));
         mCompositeDisposable.add(disposable);
     }
 }
