a3d59b36a5d63e624e7123bc64c9f66d105edc75
贾博瑄 <jiaboxuan@satcatche.com>
2018-06-07 14:18:29 +0800

    feat:更新dialog引用

--- app/src/main/java/com/jiaye/cashloan/view/home/HomeFragment.java
+++ app/src/main/java/com/jiaye/cashloan/view/home/HomeFragment.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
