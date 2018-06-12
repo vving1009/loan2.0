8268a3dd9d1a0d9f34e1c69d631377e54ac91d96
李伟 <liwei@satcatche.com>
2018-06-12 14:11:35 +0800

    fix: 修复MainActivity切换权限设置界面，再返回后页面变白的问题

--- app/src/main/java/com/jiaye/cashloan/view/main/MainActivity.java
+++ app/src/main/java/com/jiaye/cashloan/view/main/MainActivity.java
@@ -3,6 +3,7 @@ package com.jiaye.cashloan.view.main;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 
 import com.jiaye.cashloan.R;
 import com.jiaye.cashloan.view.BaseActivity;
@@ -10,20 +11,34 @@ import com.jiaye.cashloan.view.login.LoginActivity;
 
 public class MainActivity extends BaseActivity {
 
+    private MainFragment myFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            myFragment = MainFragment.newInstance();
+        } else {
+            myFragment = (MainFragment) getSupportFragmentManager().getFragment(savedInstanceState, "fragment");
+        }
         setContentView(R.layout.main_activity);
         FragmentManager fragmentManager = getSupportFragmentManager();
-        MainFragment fragment = MainFragment.newInstance();
-        fragmentManager.beginTransaction().replace(R.id.layout_content, fragment).commit();
+        fragmentManager.beginTransaction().replace(R.id.layout_content, myFragment).commit();
         Bundle bundle = getIntent().getExtras();
         if (bundle != null) {
-            String type = bundle.getString("type","default");
+            String type = bundle.getString("type", "default");
             if (type.equals("reLogin")) {
                 Intent intent = new Intent(this, LoginActivity.class);
                 startActivity(intent);
             }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (myFragment != null) {
+            getSupportFragmentManager().putFragment(outState, "fragment", myFragment);
         }
     }
 }
