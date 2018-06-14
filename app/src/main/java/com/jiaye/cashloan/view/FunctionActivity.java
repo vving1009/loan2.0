aea8cd97292a9eea1959f671ace3aed9a25d42ce
李伟 <liwei@satcatche.com>
2018-06-14 09:59:58 +0800

    fix: 修复从权限管理页面切换到个人信息页面显示内容为空的问题

--- app/src/main/java/com/jiaye/cashloan/view/FunctionActivity.java
+++ app/src/main/java/com/jiaye/cashloan/view/FunctionActivity.java
@@ -7,7 +7,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.jiaye.cashloan.R;
-import com.jiaye.cashloan.http.data.my.CreditInfo;
 import com.jiaye.cashloan.view.about.AboutFragment;
 import com.jiaye.cashloan.view.account.AccountFragment;
 import com.jiaye.cashloan.view.bankcard.BankCardFragment;
@@ -31,7 +30,7 @@ import com.jiaye.cashloan.view.vehicle.VehicleFragment;
  */
 public class FunctionActivity extends AppCompatActivity {
 
-    private static final String BUNDLE = "bundle";
+    private BaseFunctionFragment mFragment;
 
     public static void function(Activity activity, String function) {
         Intent intent = new Intent(activity, FunctionActivity.class);
@@ -44,83 +43,80 @@ public class FunctionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.function_activity);
         String function = getIntent().getStringExtra("function");
-        Bundle bundle;
-        switch (function) {
-            case "Certification":
-                // 认证中心
-                CertificationFragment certificationFragment = CertificationFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, certificationFragment).commit();
-                break;
-            case "ID":
-                // 身份认证
-                IDFragment idFragment = IDFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, idFragment).commit();
-                break;
-            case "Info":
-                // 个人资料
-                InfoFragment infoFragment = InfoFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, infoFragment).commit();
-                break;
-            case "Phone":
-                // 手机运营商
-                PhoneFragment phoneFragment = PhoneFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, phoneFragment).commit();
-                break;
-            case "Vehicle":
-                // 车辆证件
-                VehicleFragment vehicleFragment = VehicleFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, vehicleFragment).commit();
-                break;
-            case "Taobao":
-                // 淘宝
-                TaoBaoFragment taoBaoFragment = TaoBaoFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, taoBaoFragment).commit();
-                break;
-            case "File":
-                // 进件资料
-                FileFragment fileFragment = FileFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, fileFragment).commit();
-                break;
-            case "Sign":
-                // 电子签章
-                SignFragment signFragment = SignFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, signFragment).commit();
-                break;
-            case "Plan":
-                // 还款计划
-                PlanFragment planFragment = PlanFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, planFragment).commit();
-                break;
-            case "About":
-                // 关于我们
-                AboutFragment aboutFragment = AboutFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, aboutFragment).commit();
-                break;
-            case "Account":
-                //账户管理
-                AccountFragment accountFragment = AccountFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, accountFragment).commit();
-                break;
-            case "BankCard":
-                // 银行卡管理
-                BankCardFragment bankCardFragment = BankCardFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, bankCardFragment).commit();
-                break;
-            case "BindBank":
-                // 绑定银行卡
-                BindBankFragment bindBankFragment = BindBankFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, bindBankFragment).commit();
-                break;
-            case "Support":
-                // 支持的银行
-                SupportFragment supportFragment = SupportFragment.newInstance();
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, supportFragment).commit();
-                break;
-            case "Cash":
-                // 提现
-                CashFragment cashFragment = CashFragment.newInstance(getIntent().getParcelableExtra("balance"));
-                getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, cashFragment).commit();
-                break;
+        if (savedInstanceState == null) {
+            switch (function) {
+                case "Certification":
+                    // 认证中心
+                    mFragment = CertificationFragment.newInstance();
+                    break;
+                case "ID":
+                    // 身份认证
+                    mFragment = IDFragment.newInstance();
+                    break;
+                case "Info":
+                    // 个人资料
+                    mFragment = InfoFragment.newInstance();
+                    break;
+                case "Phone":
+                    // 手机运营商
+                    mFragment = PhoneFragment.newInstance();
+                    break;
+                case "Vehicle":
+                    // 车辆证件
+                    mFragment = VehicleFragment.newInstance();
+                    break;
+                case "Taobao":
+                    // 淘宝
+                    mFragment = TaoBaoFragment.newInstance();
+                    break;
+                case "File":
+                    // 进件资料
+                    mFragment = FileFragment.newInstance();
+                    break;
+                case "Sign":
+                    // 电子签章
+                    mFragment = SignFragment.newInstance();
+                    break;
+                case "Plan":
+                    // 还款计划
+                    mFragment = PlanFragment.newInstance();
+                    break;
+                case "About":
+                    // 关于我们
+                    mFragment = AboutFragment.newInstance();
+                    break;
+                case "Account":
+                    //账户管理
+                    mFragment = AccountFragment.newInstance();
+                    break;
+                case "BankCard":
+                    // 银行卡管理
+                    mFragment = BankCardFragment.newInstance();
+                    break;
+                case "BindBank":
+                    // 绑定银行卡
+                    mFragment = BindBankFragment.newInstance();
+                    break;
+                case "Support":
+                    // 支持的银行
+                    mFragment = SupportFragment.newInstance();
+                    break;
+                case "Cash":
+                    // 提现
+                    mFragment = CashFragment.newInstance(getIntent().getParcelableExtra("balance"));
+                    break;
+            }
+        } else {
+            mFragment = (BaseFunctionFragment) getSupportFragmentManager().getFragment(savedInstanceState, "fragment");
+        }
+        getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, mFragment).commit();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mFragment != null) {
+            getSupportFragmentManager().putFragment(outState, "fragment", mFragment);
         }
     }
 }
